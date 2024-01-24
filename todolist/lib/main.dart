01101c8de534ec3bf437489f84a5290845ba28e1
don <donpattamana@gmail.com>
2024-01-24 18:11:31 +0530

    update

--- todolist/lib/main.dart
+++ todolist/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,13 +8,67 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      // home: Home()
+      theme: ThemeData(
+        primarySwatch: Colors. orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: home()
     );
   }
 }
 
+class home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(appBar: AppBar(title: Text("hello world")),
+    body: inoutText()
+    );
+  }
+}
+
+
+class inoutText extends StatefulWidget {
+  const inoutText({super.key});
+
+  @override
+  State<inoutText> createState() => _inoutTextState();
+}
+
+
+class _inoutTextState extends State<inoutText> {
+  final controller = TextEditingController();
+  String text="";
+
+  void dispose(){
+    super.dispose();
+    controller.dispose();
+  }
+
+  void changeText(text){
+    if (text=="Hello"){
+      controller.clear();
+      text="";
+    }
+    setState(() {
+      this.text=text;
+    });
+    this.text=text;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      TextField(
+        controller: this.controller,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.message),labelText: "Type a message:"),
+        onChanged: (text)=>this.changeText(text),
+      ),Text(text)
+      ],
+        );
+  }
+}
